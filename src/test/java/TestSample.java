85a2bfe568b7d293f3a2d56270e71f60aeeec46f
nilam <nilamlatkar@gmail.com>
2023-09-14 17:27:44 +0530

    GitStuff Changes again

--- src/test/java/TestSample.java
+++ src/test/java/TestSample.java
@@ -14,6 +14,10 @@ public class TestSample {
       System.out.println(data.get(3));
 // for selenium
       //driver.findElement(by.xpath("hdg")).sendkeys(data.get(1));
+      
+      System.out.println(data.get(0));
+      System.out.println(data.get(1));
+      System.out.println(data.get(2));
 
 	}
 
